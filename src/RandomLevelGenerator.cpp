35e1253b0f2b3b5dd80737c91fbc41b7d70c7956
Hua1704 <127390090+Hua1704@users.noreply.github.com>
2024-01-01 22:24:49 +0700

    add case forestriver

--- src/RandomLevelGenerator.cpp
+++ src/RandomLevelGenerator.cpp
@@ -77,6 +77,13 @@ void RandomLevelGenerator::generateNewLevel() {
 			isLastRoadSafe = false;
 			isLastRoadRiver = false;
 			break;
+		case 5:
+			numVehicle = 2;
+			newSpeed = baseSpeed * (1.0 + 0.2 * static_cast<float>(difficulty));
+			roadVector.push_back(new ForestRiver(numVehicle, newSpeed, i * roadHeight, i * roadHeight + roadHeight));
+			isLastRoadSafe = false;
+			isLastRoadRiver = true;
+			break;
 		default:
 			break;
 		}
